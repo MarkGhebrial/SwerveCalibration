0fce64bc4d2a0118b007926092e7fa74463d2e8d
MarkGhebrial <mghebrialjr@icloud.com>
2022-02-11 18:04:02 -0800

    Quick and dirty tool to calibrate swerve modules

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * The VM is configured to automatically run this class, and to call the run()
  * function when the
@@ -12,6 +17,12 @@ package frc.robot;
  * project, you must also update the build.gradle file in the project.
  */
 public class Robot extends EducationalRobot {
+    private static final int CANCODER_CAN_ID = 10;
+
+    public static final XboxController xboxController = new XboxController(2);
+
+    private CANCoder canCoder = new CANCoder(CANCODER_CAN_ID);
+
     /**
      * This function is run when the robot is first started up and should be used
      * for any
@@ -19,10 +30,22 @@ public class Robot extends EducationalRobot {
      */
     @Override
     public void robotInit() {
+        canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+        canCoder.configMagnetOffset(0);
     }
 
     /** This function is run when the robot is enabled. */
     @Override
     public void run() {
+        System.out.println("Move the swerve module to its zero position, then press A on the Xbox controller");
+
+        while (!xboxController.getAButton()) {}
+
+        double absolutePosition = canCoder.getAbsolutePosition();
+        int canCoderSlotValue = (int) (absolutePosition * 100 + 0.5);
+
+        System.out.println("Setting custom parameter 0 to " + canCoderSlotValue + " (absolute position = " + absolutePosition + " degrees)");
+
+        canCoder.configSetCustomParam(canCoderSlotValue, 0);
     }
 }
