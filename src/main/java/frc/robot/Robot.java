c131b1bfb4603f7485eae0191fd996cefad9d52e
Mark Ghebrial <mghebrialjr@icloud.com>
2022-04-04 20:34:52 -0700

    Calibrate multiple modules at once

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,35 +17,35 @@ import edu.wpi.first.wpilibj.XboxController;
  * project, you must also update the build.gradle file in the project.
  */
 public class Robot extends EducationalRobot {
-    private static final int CANCODER_CAN_ID = 10;
+    private static final int[] CANCODER_CAN_IDS = { 50, 51, 52, 53 };
 
     public static final XboxController xboxController = new XboxController(2);
 
-    private CANCoder canCoder = new CANCoder(CANCODER_CAN_ID);
-
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any
-     * initialization code.
-     */
     @Override
     public void robotInit() {
-        canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
-        canCoder.configMagnetOffset(0);
     }
 
-    /** This function is run when the robot is enabled. */
     @Override
     public void run() {
-        System.out.println("Move the swerve module to its zero position, then press A on the Xbox controller");
+        System.out.println("Move the swerve modules to their zero positions, then press A on the Xbox controller");
 
         while (!xboxController.getAButton()) {}
 
-        double absolutePosition = canCoder.getAbsolutePosition();
-        int canCoderSlotValue = (int) (absolutePosition * 100 + 0.5);
+        for (int id : CANCODER_CAN_IDS) {
+            CANCoder canCoder = new CANCoder(id);
+            canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+            canCoder.configMagnetOffset(0);
 
-        System.out.println("Setting custom parameter 0 to " + canCoderSlotValue + " (absolute position = " + absolutePosition + " degrees)");
+            double absolutePosition = canCoder.getAbsolutePosition();
+            int canCoderSlotValue = (int) (absolutePosition * 100 + 0.5);
 
-        canCoder.configSetCustomParam(canCoderSlotValue, 0);
+            System.out.println(
+                "Setting custom parameter on encoder" + id +
+                " to " + canCoderSlotValue +
+                " (absolute position = " + absolutePosition + " degrees)"
+            );
+
+            canCoder.configSetCustomParam(canCoderSlotValue, 0);
+        }
     }
 }
